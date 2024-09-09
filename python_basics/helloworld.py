bac2e9ee4e8f082310484b82239a715e2b28007e
Aurelia Wurfbain <awurfbain@gmail.com>
2024-09-09 21:40:15 +0200

    Assignment 1.4 Git & Python

--- python_basics/helloworld.py
+++ python_basics/helloworld.py
@@ -1,1 +1,5 @@
-print("Hello World")
+username = input("what is your name? ")
+print("Hello", username)
+
+#Shift + enter line 1, vul je naam in in de terminal onderaan.
+#Shift + enter line 2. VSC begroet je.
